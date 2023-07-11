05674178d162f6c008415d200ae8852a4624b375
magicyang <2190763151@qq.com>
2023-07-11 16:21:06 +0800

    third

--- p1/src/Main.java
+++ p1/src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("hello world first version");
         //second
         System.out.println("second");
+        //third
+        System.out.println("third");
     }
 }
